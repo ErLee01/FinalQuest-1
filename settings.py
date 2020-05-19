1a1edd2fe7769bdad2888549cf0a6d4df87c3026
ErLee01 <62352270+ErLee01@users.noreply.github.com>
2020-05-19 11:37:53 -0700

    Comments and Finals

--- settings.py
+++ settings.py
@@ -4,16 +4,19 @@ BLACK = (0, 0, 0)
 DARKGREY = (40, 40, 40)
 LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+AQUA = (0, 255, 255)
 YELLOW = (255, 255, 0)
-
+RED = (255, 0, 0)
+ 
 # game settings
 WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
-TITLE = "Tilemap Demo"
+TITLE = "Our Game"
+#Background color is DarkGrey
 BGCOLOR = DARKGREY
 
+#Defines how big the tiles are 
 TILESIZE = 32
 GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
